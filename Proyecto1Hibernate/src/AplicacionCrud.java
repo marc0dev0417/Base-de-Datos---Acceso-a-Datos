b99e34fa460d0861d1d5b90bcdda9c497b973980
Marco Benegas <180102@iessierradeguara.com>
2021-12-01 10:20:46 +0100

    Practica casi terminada (Faltan gestionar algunos errores de salida

--- Proyecto1Hibernate/src/AplicacionCrud.java
+++ Proyecto1Hibernate/src/AplicacionCrud.java
@@ -5,6 +5,8 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
+import org.hibernate.ObjectNotFoundException;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -16,6 +18,7 @@ import primero.SessionFactoryUtil;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.sql.Date;
+import java.util.List;
 import java.awt.event.ActionEvent;
 import primero.Departamentos;
 import primero.Empleados;
@@ -53,13 +56,26 @@ public class AplicacionCrud {
 	 */
 	public AplicacionCrud() {
 		initialize();
+		cargarDirectores();
 	}
 	public void cargarDirectores() {
+		SessionFactory sesion = SessionFactoryUtil.getSessionFactory();
+		Session session = sesion.openSession();
+		
+		Empleados empleados = new Empleados();
+		
+		
 		
 		comboDirectores.removeAllItems();
 		
+		String hql = "from Empleados";
+		Query consulta = session.createQuery(hql);
 		
-		String hql = "from empleados where empleado.dir";
+		List<Empleados> lista = consulta.list();
+		
+		for(Empleados empleado : lista) {
+			comboDirectores.addItem(empleado.getEmpNo()+"/"+empleado.getApellido());
+		}
 	}
 	/**
 	 * Initialize the contents of the frame.
@@ -121,23 +137,34 @@ public class AplicacionCrud {
 				if(e.getSource() != null) {
 					SessionFactory sesion = SessionFactoryUtil.getSessionFactory();
 					Session session = sesion.openSession();
-					
+				
+					try {
 					Empleados empleados = new Empleados();
 					empleados = (Empleados)session.load(Empleados.class, (short) (Short.valueOf(txtNumEmpleado.getText())));
 					
 					JOptionPane.showMessageDialog(null, "Numero Empleado: "+empleados.getEmpNo()+"\n"+"Apellido: "+empleados.getApellido()+"\n"+"Oficio: "+empleados.getOficio());
-				}
+						}catch(ObjectNotFoundException error) {
+							JOptionPane.showMessageDialog(null, "No existe el empleado");
+						}catch(NumberFormatException error) {
+							
+						}
+					}
 			}
 		});
 		btnConsultar.setBounds(298, 21, 169, 23);
 		frame.getContentPane().add(btnConsultar);
 		
 		JButton btnSalir = new JButton("Salir");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(-1);
+			}
+		});
 		btnSalir.setBounds(102, 267, 89, 23);
 		frame.getContentPane().add(btnSalir);
 		
 		JButton btnLimpiar = new JButton("Limpiar");
-		btnLimpiar.setBounds(291, 267, 89, 23);
+		btnLimpiar.setBounds(201, 267, 89, 23);
 		frame.getContentPane().add(btnLimpiar);
 		
 		JComboBox comboDepartamentos = new JComboBox();
@@ -159,8 +186,6 @@ public class AplicacionCrud {
 					System.out.println("funciona el insert");
 				
 					String idEmpleadoDirector;
-					
-				
 					
 						//System.out.println(comboDepartamentos.getSelectedItem().toString());
 						
@@ -201,7 +226,7 @@ public class AplicacionCrud {
 						System.out.println("Fecha mal introducida");
 					}
 					session.save(empleado);
-					
+					cargarDirectores();
 					try {
 						tx.commit();
 						comboDirectores.addItem(txtNumEmpleado.getText()+"/"+txtApellido.getText());
@@ -222,9 +247,7 @@ public class AplicacionCrud {
 					e.printStackTrace();
 				}
 					session.close();
-					
 				}
-				
 			}
 		});
 		btnInsertar.setBounds(102, 233, 89, 23);
@@ -238,5 +261,86 @@ public class AplicacionCrud {
 		textFechaAlta.setBounds(327, 146, 119, 20);
 		frame.getContentPane().add(textFechaAlta);
 		textFechaAlta.setColumns(10);
+		
+		JButton btnEliminar = new JButton("Eliminar");
+		btnEliminar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evento) {
+				
+			if(evento.getSource() != null) {
+				
+			
+				SessionFactory sesion = SessionFactoryUtil.getSessionFactory();
+				Session session = sesion.openSession();
+				Transaction tx = session.beginTransaction();
+
+				Empleados empleado = (Empleados) session.load(Empleados.class, (short) Short.valueOf(txtNumEmpleado.getText()));
+
+				try {
+					session.delete(empleado); // elimina el objeto
+					tx.commit();
+					cargarDirectores();
+					JOptionPane.showMessageDialog(null, "Empleado borrado");
+				} catch (ObjectNotFoundException o) {
+					JOptionPane.showMessageDialog(null, "No existe el departamento");
+				} catch (ConstraintViolationException c) {
+					JOptionPane.showMessageDialog(null, "No se puede eliminar tiene empleados");
+				}catch (Exception e) {
+					System.out.println("ERROR NO CONTROLADO....");
+					e.printStackTrace();
+				}
+				session.close();
+			}
+			}
+		});
+		btnEliminar.setBounds(201, 233, 89, 23);
+		frame.getContentPane().add(btnEliminar);
+		
+		JButton btnActualizar = new JButton("Modificar");
+		btnActualizar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evento) {
+				SessionFactory sesion = SessionFactoryUtil.getSessionFactory();
+				Session session = sesion.openSession();
+				Transaction tx = session.beginTransaction();
+
+				Empleados em = new Empleados();
+				try {
+					em = (Empleados) session.load(Empleados.class, (short) Short.valueOf(txtNumEmpleado.getText()));
+					
+					em.setApellido(txtApellido.getText());
+					em.setOficio(txtOficio.getText());
+					
+					
+					float NuevoSalario = Float.valueOf(txtSalario.getText());
+					em.setSalario(NuevoSalario);
+					
+					em.setComision(Float.valueOf(txtComision.getText()));
+					
+					String[] dividirNumeroDepartamento = comboDepartamentos.getSelectedItem().toString().split("/");
+					
+					Departamentos dep = (Departamentos) session.get(Departamentos.class, (byte) Byte.valueOf(dividirNumeroDepartamento[0]));
+					if (dep == null) {
+						System.out.println("El departamento no existe");
+					} else {
+						em.setDepartamentos(dep);
+						
+						session.update(em); // modifica el objeto
+						tx.commit();
+						cargarDirectores();
+					}
+
+				} catch (ObjectNotFoundException o) {
+					JOptionPane.showMessageDialog(null, "No existe el empleado");
+				} catch (ConstraintViolationException c) {
+					JOptionPane.showMessageDialog(null, "No se puede asignar un departamento que no existe");
+				} catch (Exception e) {
+					System.out.println("ERROR NO CONTROLADO....");
+					e.printStackTrace();
+				}
+				session.close();
+				
+			}
+		});
+		btnActualizar.setBounds(298, 233, 89, 23);
+		frame.getContentPane().add(btnActualizar);
 	}
 }
